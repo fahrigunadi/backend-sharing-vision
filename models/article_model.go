8dbf7a5463af00b59ad11abe8e44b81b079c8c2f
Fahri Gunadi <fahri@fahrigunadi.dev>
2024-08-17 13:46:50 +0700

    fix: column created_date and updated_date

--- models/article_model.go
+++ models/article_model.go
@@ -4,10 +4,10 @@ import "time"
 
 type Article struct {
 	Id          uint64    `gorm:"primaryKey" json:"id"`
-	Title       string    `gorm:"type:varchar(200)" json:"title"`
-	Content     string    `gorm:"type:text" json:"content"`
-	Category    string    `gorm:"type:varchar(100)" json:"category"`
-	CreatedDate time.Time `gorm:"type:timestamp;autoCreateTime" json:"created_date"`
-	UpdatedDate time.Time `gorm:"type:timestamp;autoCreateTime;autoUpdateTime" json:"updated_date"`
-	Status      string    `gorm:"type:varchar(100)" json:"status"`
+	Title       string    `gorm:"type:varchar(200);not null" json:"title"`
+	Content     string    `gorm:"type:text;not null" json:"content"`
+	Category    string    `gorm:"type:varchar(100);not null" json:"category"`
+	CreatedDate time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"created_date"`
+	UpdatedDate time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP" json:"updated_date"`
+	Status      string    `gorm:"type:varchar(100);not null" json:"status"`
 }
